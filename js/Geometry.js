86559b66d4b5ffad45efd2b222d008e812f4d50b
Marcos Hernández <marcoslhc@gmail.com>
2013-06-11 18:43:24 -0400

    Separate class to use with requireJS

---
+++ js/Geometry.js
@@ -0,0 +1,10 @@
+define(function (){
+  // ==================
+  // = Class Geometry =
+  // ==================
+  var Geometry = function() {
+    this.vertices = [];
+    this.triangles = [];
+  }
+  return Geometry;
+});
---
+++ js/Triangle.js
@@ -0,0 +1,41 @@
+define(['Vertex', 'Scales'],function(Vertex, scales){
+  // ==================
+  // = Class Triangle =
+  // ==================  
+  var Triangle = function(a,b,c){
+    this.a = a || new Vertex();
+    this.b = b || new Vertex();
+    this.c = c || new Vertex();
+    
+    this.vertices = [this.a,this.b,this.c];
+    this.centroid = new paper.Point();
+    this.color = new  paper.Color({hue:scales.randomInRange(190,220),saturation:1,brightness:1})
+    
+    this.polygon = new  paper.Path(this.vertices);
+    this.polygon.closed = true;
+    this.polygon.style.fillColor = this.color
+    this.polygon.style.strokeColor =  this.color;
+    this.polygon.style.strokeWidth = 2;
+    this.polygon.style.strokeJoin = 'round'
+    
+    this.computeCentroid();
+    
+    return this;
+  
+  }
+  Triangle.prototype = {
+    
+    setColor : function(color){
+      this.color = color || new paper.Color();
+      this.polygon.style.fillColor = this.color;
+      this.polygon.style.strokeColor = this.color;
+    },
+    
+    computeCentroid: function(){
+      this.centroid.x = (this.a.x + this.b.x + this.c.x)/3;
+      this.centroid.y = (this.a.y + this.b.y + this.c.y)/3;
+    }
+    
+  }
+  return Triangle;
+})
