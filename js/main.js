ac9c99181433c923b1b9f06f5762fff81ff426e6
Marcos Hernández <marcoslhc@gmail.com>
2013-06-11 21:58:50 -0400

    Use RequireJS

--- js/main.js
+++ js/main.js
@@ -6,32 +6,16 @@
 //  Created by Marcos Hernández on 2013-06-12.
 //  Copyright 2013 Marcos Hernández. All rights reserved.
 // 
-
-var MESH = {
-  speed : 0.001,
-  xRange: 16,
-  yRange:18,
-  xSegments:16,
-  ySegments:9
-}
-Math.PIM2 = Math.PI*2
-Math.PID2 = Math.PI/2
-
-window.onload = function(){
+require(['paper','Plane'],function(s,Plane){
   var canvas = document.getElementById('tutorial')
   paper.setup(canvas);
-  var geometry = new init();
+  var geometry = new Plane();
   var now, start = Date.now();
-  
-  
   paper.view.draw();
   paper.view.onFrame = function(){
     now = Date.now() - start;
     geometry.update(now);
     
   }
+});
 
-  function mouseMove(){
-  
-  }  
-}
