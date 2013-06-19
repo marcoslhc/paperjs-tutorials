4f5360a2fcd81da638bceda85210860c5036e4d5
Marcos Hernández <marcoslhc@gmail.com>
2013-06-19 13:27:49 -0400

    Now the triangles are made with variable size

--- js/Plane2.js
+++ js/Plane2.js
@@ -26,16 +26,34 @@ define(['Geometry','Triangle','Scales','Vertex','T'], function(Geometry, Triangl
   
   }
   Plane.prototype.renderTriangles = function (){
-    var x,y,v0,v1,v2,v3,t0,t1;
-    for(x = 0; x<this.xSegments;x++){
-      for(y=0;y<this.ySegments;y++){
+    var x,y,v0,v1,v2,v3,vc,t0,t1,t2,t3;
+    
+    var CalcSize = function(){
+      var sizeIdx, sizes = [1, 2, 3, 5, 8];
+      sizeIdx = ~~(Math.random()*(sizes.length));
+      return sizes[sizeIdx];
+    }
+    console.log(this.xSegments,this.ySegments)
+    for(x = 0; x < this.xSegments; x++){
+      for(y = 0; y < this.ySegments; y++){
+        
+        size = CalcSize();
+                
+        if( x + size > this.xSegments || y + size > this.ySegments ){
+          size = Math.min(this.xSegments - x,this.ySegments-y);
+        }
+        
         v0 = this.vertices[x+0][y+0];
-        v1 = this.vertices[x+0][y+1];
-        v2 = this.vertices[x+1][y+0];
-        v3 = this.vertices[x+1][y+1];
-        t0 = new Triangle(v0,v1,v2);
-        t1 = new Triangle(v2,v1,v3);
-        this.triangles.push(t0,t1);
+        v1 = this.vertices[x+0][y+size];
+        v2 = this.vertices[x+size][y+0];
+        v3 = this.vertices[x+size][y+size];
+        var rect = new paper.Rectangle(v0,v3);
+        vc = rect.center;
+        t0 = new Triangle(v0,v2,vc);
+        t1 = new Triangle(v2,v3,vc);
+        t2 = new Triangle(v3,v1,vc);
+        t3 = new Triangle(v1,v0,vc);
+        this.triangles.push(t0,t1,t2,t3);
       }
     }
   }
@@ -48,7 +66,7 @@ define(['Geometry','Triangle','Scales','Vertex','T'], function(Geometry, Triangl
       this.vertices.push([]);
       for (y = 0; y <= this.ySegments; y++) {
         vertex = new Vertex(
-	offsetX + x * this.segmentSize.width,
+        offsetX + x * this.segmentSize.width,
         offsetY + y * this.segmentSize.height);
         this.vertices[x].push(vertex);
         //this.vertices.push(vertex);
@@ -56,7 +74,7 @@ define(['Geometry','Triangle','Scales','Vertex','T'], function(Geometry, Triangl
     }
   }
   Plane.prototype.update = function(now){
-    
+
   }
   return Plane;
 })
