d82055a07666c4e9ef05cdeb87d204a6478638cd
Marcos Hernández <marcoslhc@gmail.com>
2013-07-09 17:51:07 -0400

    use a object to store colors

---
+++ js/colors.js
@@ -0,0 +1,45 @@
+define(['paper'], function (s) {
+  return {
+    blue: new paper.Color({
+      hue: 222,
+      saturation: 0.74,
+      brightness: 0.54
+    }),
+    darkBlue: new paper.Color({
+      hue: 222,
+      saturation: 0.74,
+      brightness: 0.18
+    }),
+    lilly:new paper.Color({
+      hue: 235,
+      saturation: 0.10,
+      brightness: 0.87
+    }),
+    darkLilly:new paper.Color({
+      hue: 235,
+      saturation: 0.10,
+      brightness: 0.51
+    }),
+    brick: new paper.Color({
+      hue: 4,
+      saturation: 0.75,
+      brightness: 0.91
+    }),
+    oldBrick: new paper.Color({
+      hue: 4,
+      saturation: 0.75,
+      brightness: 0.55
+    }),
+    eggShell:new paper.Color({
+      hue: 30,
+      saturation: 0,
+      brightness: 1
+    }),
+    rotEggShell:new paper.Color({
+      hue: 30,
+      saturation: 0,
+      brightness: 0.76
+    }), 
+    
+  }
+})
