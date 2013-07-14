fe52b9c32afdb1a0426db91124751dbc3314a8cd
Marcos Hernández <marcoslhc@gmail.com>
2013-07-14 18:45:54 -0400

    Arguments helper module. Right now consist in only argument validation

---
+++ js/Args.js
@@ -0,0 +1,17 @@
+define(function () {
+    var Args = function () {}
+    var errorMessages = {
+	"TypeError":"Tipo incorrecto"
+    }
+    Args.type = function (o) {
+	return !!o && Object.prototype.toString.call(o).match(/(\w+)\s(\w+)/)[2];
+    }
+    Args.process = function(arg,type,cb){
+	if(Args.type(arg) === type) {
+	    return cb(arg);
+	}else{
+	    throw new TypeError(errorMessages["TypeError"])
+	}
+    }
+    return Args;
+})
