75f8ebaa4829a59cb89011cff3ed31fde882477d
Marcos Hernández <marcoslhc@gmail.com>
2013-07-14 19:15:07 -0400

    add jQuery

--- js/config.js
+++ js/config.js
@@ -2,11 +2,15 @@ requirejs.config({
   baseUrl:"/js/",
   urlArgs:"bust="+(new Date()).getTime(),
   paths:{
-    'paper':'paperjs-nightly/lib/paper'
+    'paper':'paperjs-nightly/lib/paper',
+    'jquery':'http://code.jquery.com/jquery-1.10.1.min'
   },
   shim:{
     'paper':{
       exports: 'paper'
+    },
+    'jquery':{
+	exports:'jquery'
     }
   }
 });
