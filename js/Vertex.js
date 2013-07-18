d207297538478b894172389f76515eed149351c8
Marcos Hernández <marcoslhc@gmail.com>
2013-07-18 10:15:20 -0400

    Correct prototype chain

--- js/Vertex.js
+++ js/Vertex.js
@@ -5,6 +5,8 @@ define(['Scales'],function (scales) {
   // ================
   var Vertex = function(x,y){
     paper.Point.call(this,x,y);
+    Vertex.prototype = new paper.Point();
+    Vertex.prototype.constructor = Vertex;
     this.anchor = new paper.Point(this.x,this.y)
     this.time = scales.randomInRange(0,Math.PI*2);
     this.step = new paper.Point(
